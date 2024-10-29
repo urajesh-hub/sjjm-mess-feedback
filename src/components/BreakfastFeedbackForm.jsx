eeed3d06d024debaab01596c3c8d8bbf83b45b91
urajesh-hub <rurmails@gmail.com>
2024-10-30 01:42:49 +0530

    Update BreakfastFeedbackForm.jsx

--- src/components/BreakfastFeedbackForm.jsx
+++ src/components/BreakfastFeedbackForm.jsx
@@ -1,11 +1,14 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import { db } from './firebaseConfig';
 import { collection, addDoc } from 'firebase/firestore';
 import './StarRating.css';
 
+
 const BreakfastFeedbackForm = () => {
+ 
   const [formData, setFormData] = useState({
-   
+
     date: '',
     categories: [],
     hospitality: '',
@@ -70,7 +73,7 @@ const BreakfastFeedbackForm = () => {
       await addDoc(collection(db, 'breakfastFeedback'), { ...formData, date: currentDate });
       alert('Breakfast feedback submitted successfully');
       setFormData({
-        
+
         date: '',
         categories: [],
         hospitality: '',
@@ -82,6 +85,7 @@ const BreakfastFeedbackForm = () => {
         sideDishComment: '',
         additionalComments: ''
       });
+      
     } catch (error) {
       console.error('Error submitting feedback', error);
       alert('There was an error submitting your feedback.');
@@ -89,28 +93,29 @@ const BreakfastFeedbackForm = () => {
   };
 
   // Options
-  const mainDishes = ['ஆப்பம்', 'பூரி', 'தோசை', 'கோதுமை தோசை', 'இட்லி', 'வெண் பொங்கல்'];
-  const sideDishes = ['தேங்காய் பால்', 'சாம்பார்', 'குருமா','தக்காளி சட்னி','தேங்காய் சட்னி','வடை'];
-  const categories = ['SJJ- Staff', 'Guest', 'Erector/Service Engineer', 'Auditor', 'Others'];
-  const hospitalityOptions = ['சுமார்', 'மோசம்', 'நன்று', 'அருமை'];
-  const cleanlinessOptions = ['சுமார்', 'மோசம்', 'நன்று', 'அருமை'];
+  const mainDishes = ['APPAM- ஆப்பம்', 'PURI- பூரி', 'DOSA- தோசை', 'WHEAT DOSA- கோதுமை தோசை', 'IDLY- இட்லி', 'WHITE PONGAL- வெண் பொங்கல்'];
+  const sideDishes = ['COCONUT MILK- தேங்காய் பால்', 'SAMBAR- சாம்பார்', 'KURUMA- குருமா', 'TOMATO CHUTNEY- தக்காளி சட்னி', 'COCONUT CHUTNEY- தேங்காய் சட்னி', 'VADAI- வடை'];
+  const categories = ['COMPANY STAFF', 'GUEST', 'ERECTOR/SERVICE ENGINEER', 'AUDITOR', 'OTHERS'];
+  const hospitalityOptions = ['AVERAGE - சுமார்', 'POOR - மோசம்', 'GOOD - நன்று', 'EXCELLENT - அருமை'];
+  const cleanlinessOptions = ['AVERAGE - சுமார்', 'POOR - மோசம்', 'GOOD - நன்று', 'EXCELLENT - அருமை'];
+
 
   return (
     <div className="container my-4 ">
       <div className="card">
         <div className="card-header bg-primary text-white">
-        <h6 className="card-title mb-0 text-center fw-bold">BREAKFAST FEEDBACK FORM</h6>
+          <h6 className="card-title mb-0 text-center fw-bold">BREAKFAST FEEDBACK FORM</h6>
         </div>
         <div className="card-body">
           {/* Display Current Date and Time in the desired format */}
           <h6 className="text-center fw-bold bg-light text-dark">{currentDateTime}</h6>
           <form onSubmit={handleSubmit}>
-            
 
-           <div className="card mb-3 border-secondary">
-            <img src="/images/category.jpg" alt="Category" className="card-img-top" />
+            {/*Category*/}
+            <div className="card mb-3 border-secondary">
               <div className="card-body">
-                <label className="form-label"><strong>3. Categories</strong></label>
+                <img src="/images/category.jpg" alt="Category" className="card-img-top" />
+                <label className="form-label " style={{ fontSize: '0.9rem', marginBottom: '0.01rem', textTransform: "uppercase" }}><strong>VISTORS CATEGORY  </strong>   </label>
                 {categories.map((category, index) => (
                   <div key={index} className="form-check">
                     <input
@@ -129,11 +134,14 @@ const BreakfastFeedbackForm = () => {
                 ))}
               </div>
             </div>
-
+            {/*Hospility*/}
             <div className="card mb-3 border-secondary">
-            <img src="/images/hospitality.jpg" alt="Hospitality" className="card-img-top" />
               <div className="card-body">
-                <label className="form-label"><strong>4.  உணவு பரிமாறுபவர்களின் விருந்தோம்பல் எவ்வாறு இருந்தது? </strong></label>
+                <img src="/images/hospitality.jpg" alt="Hospitality" className="card-img-top" />
+                <label className="form-label " style={{ fontSize: '0.9rem', marginBottom: '0.01rem' }}><strong>HOW WAS THE SERVICE OF THE CATERERS? </strong>
+                  <p className='mt-1' style={{ fontSize: '0.8rem' }}>உணவு பரிமாறுபவர்களின் விருந்தோம்பல் எவ்வாறு இருந்தது?</p>
+                </label>
+
                 {hospitalityOptions.map((option, index) => (
                   <div key={index} className="form-check">
                     <input
@@ -153,10 +161,15 @@ const BreakfastFeedbackForm = () => {
               </div>
             </div>
 
+            {/*mainfood*/}
             <div className="card mb-3 border-secondary">
-            <img src="/images/main_dish.jpg" alt="Main Dish" className="card-img-top" />
+
               <div className="card-body">
-                <label className="form-label"><strong>5. நீங்கள் சாப்பிட்ட காலை உணவு ?</strong> </label>
+                <img src="/images/main_dish.jpg" alt="Main Dish" className="card-img-top" />
+                <label className="form-label " style={{ fontSize: '0.9rem', marginBottom: '0.01rem', textTransform: "uppercase" }}><strong>What breakfast did you eat? ? </strong>
+                  <p className='mt-1' style={{ fontSize: '0.9rem' }}>நீங்கள் சாப்பிட்ட காலை உணவு ?</p>
+                </label>
+
                 {mainDishes.map((dish, index) => (
                   <div key={index} className="form-check">
                     <input
@@ -168,7 +181,7 @@ const BreakfastFeedbackForm = () => {
                       checked={formData.mainDish.includes(dish)}
                       onChange={handleChange}
                     />
-                    <label className="form-check-label" htmlFor={`mainDish-${dish}`}>
+                    <label className="form-check-label" style={{ fontSize: '0.9rem', marginBottom: '0.01rem' }} htmlFor={`mainDish-${dish}`}>
                       {dish}
                     </label>
                   </div>
@@ -176,10 +189,16 @@ const BreakfastFeedbackForm = () => {
               </div>
             </div>
 
+            {/*mainfood RATING*/}
             <div className="card mb-3 border-secondary">
-            <img src="/images/rating.jpg" alt="Side Dish Rating" className="card-img-top" />
+
               <div className="card-body">
-                <label className="form-label"><strong>6. காலை உணவின்  சுவை ( ஆப்பம்,  பூரி, தோசை,கோதுமை தோசை, இட்லி, வெண் பொங்கல்) எவ்வாறு இருந்தது? </strong></label>
+                <img src="/images/rating.jpg" alt="Side Dish Rating" className="card-img-top " />
+                <label className="form-label " style={{ fontSize: '0.9rem', marginBottom: '0.01rem', textTransform: "uppercase" }}><strong>How was the taste of breakfast ? </strong>
+                  <p className='mt-1' style={{ fontSize: '0.9rem' }}>காலை உணவின்  சுவை ( ஆப்பம்,  பூரி, தோசை,கோதுமை தோசை, இட்லி, வெண் பொங்கல்) எவ்வாறு இருந்தது? ?</p>
+                </label>
+
+
                 <div className="star-rating">
                   {[1, 2, 3, 4, 5].map((star) => (
                     <span
@@ -193,11 +212,14 @@ const BreakfastFeedbackForm = () => {
                 </div>
               </div>
             </div>
-
+            {/*sidedish */}
             <div className="card mb-3 border-secondary">
-            <img src="/images/side_dish.jpg" alt="Side Dish" className="card-img-top" />
               <div className="card-body">
-                <label className="form-label"><strong>7. காலை உணவின் பக்க உணவு ?</strong></label>
+                <img src="/images/side_dish.jpg" alt="Side Dish" className="card-img-top" />
+                <label className="form-label " style={{ fontSize: '0.9rem', marginBottom: '0.01rem', textTransform: "uppercase" }}><strong>A side dish for breakfast? ? </strong>
+                  <p className='mt-1' style={{ fontSize: '0.9rem' }}>காலை உணவின் பக்க உணவு ??</p>
+                </label>
+
                 {sideDishes.map((dish, index) => (
                   <div key={index} className="form-check">
                     <input
@@ -209,18 +231,21 @@ const BreakfastFeedbackForm = () => {
                       checked={formData.sideDish.includes(dish)}
                       onChange={handleChange}
                     />
-                    <label className="form-check-label" htmlFor={`sideDish-${dish}`}>
+                    <label className="form-check-label" style={{ fontSize: '0.9rem', marginBottom: '0.01rem' }} htmlFor={`sideDish-${dish}`}>
                       {dish}
                     </label>
                   </div>
                 ))}
               </div>
             </div>
-
+            {/*sidedish Taste */}
             <div className="card mb-3 border-secondary">
-            <img src="/images/rating.jpg" alt="Side Dish Rating" className="card-img-top" />
+              <img src="/images/rating.jpg" alt="Side Dish Rating" className="card-img-top" />
               <div className="card-body">
-                <label className="form-label"><strong>8. பக்க  உணவின்  சுவை (தேங்காய் பால், சாம்பார், குருமா, தக்காளி சட்னி, தேங்காய் சட்னி) எவ்வாறு இருந்தது?</strong></label>
+                <label className="form-label " style={{ fontSize: '0.9rem', marginBottom: '0.01rem', textTransform: "uppercase" }}><strong>How was the taste of SIDE DISH ? </strong>
+                  <p className='mt-1' style={{ fontSize: '0.9rem' }}>பக்க  உணவின்  சுவை (தேங்காய் பால், சாம்பார், குருமா, தக்காளி சட்னி, தேங்காய் சட்னி) எவ்வாறு இருந்தது?</p>
+                </label>
+
                 <div className="star-rating">
                   {[1, 2, 3, 4, 5].map((star) => (
                     <span
@@ -234,11 +259,15 @@ const BreakfastFeedbackForm = () => {
                 </div>
               </div>
             </div>
-
+            {/*Clean */}
             <div className="card mb-3 border-secondary">
-            <img src="/images/cleanliness.jpg" alt="Cleanliness" className="card-img-top" />
+
               <div className="card-body">
-                <label className="form-label"><strong>9. உணவு உண்ணும் இடத்தில் சுத்தம் எவ்வாறு இருந்தது?</strong></label>
+                <img src="/images/cleanliness.jpg" alt="Cleanliness" className="card-img-top" />
+                <label className="form-label " style={{ fontSize: '0.8rem', marginBottom: '0.01rem' }}><strong>HOW WAS THE CLEANLINESS OF THE EATING AREA? </strong>
+                  <p className='mt-1' style={{ fontSize: '0.8rem' }}>உணவு உண்ணும் இடத்தில் சுத்தம் எவ்வாறு இருந்தது?</p>
+                </label>
+
                 {cleanlinessOptions.map((option, index) => (
                   <div key={index} className="form-check">
                     <input
@@ -250,18 +279,20 @@ const BreakfastFeedbackForm = () => {
                       checked={formData.cleanliness === option}
                       onChange={handleChange}
                     />
-                    <label className="form-check-label" htmlFor={`cleanliness-${option}`}>
+                    <label className="form-check-label" style={{ fontSize: '0.9rem', marginBottom: '0.01rem' }} htmlFor={`cleanliness-${option}`}>
                       {option}
                     </label>
                   </div>
                 ))}
               </div>
             </div>
-
-            <div className="card mb-3">
+            {/*Side Dish Commands */}
+            <div className="card mb-3 border-secondary">
               <div className="card-body">
-                <label htmlFor="sideDishComment" className="form-label"><strong>10. Side Dish Comment - Any specific feedback on the taste of the side dish (coconut milk, sambar, kuruma, tomato chutney, coconut chutney)?</strong></label>
-                <p> பக்க உணவின்  சுவையைப் பற்றி  (தேங்காய் பால், சாம்பார், குருமா, தக்காளி சட்னி, தேங்காய் சட்னி)  ஏதேனும் குறிப்பிட்ட கருத்து உள்ளதா?</p>
+                <label htmlFor="sidedishComments" className="form-label " style={{ fontSize: '0.9rem', marginBottom: '0.01rem', textTransform: "uppercase" }}><strong>Side Dish Comment - Any specific feedback on the taste of the side dish ? </strong>
+                  <p className='mt-1' style={{ fontSize: '0.8rem' }}>பக்க உணவின்  சுவையைப் பற்றி  (தேங்காய் பால், சாம்பார், குருமா, தக்காளி சட்னி, தேங்காய் சட்னி)  ஏதேனும் குறிப்பிட்ட கருத்து உள்ளதா?</p>
+                </label>
+
                 <textarea
                   className="form-control"
                   id="sideDishComment"
@@ -272,11 +303,15 @@ const BreakfastFeedbackForm = () => {
               </div>
             </div>
 
+            {/*additinal Commands */}
             <div className="card mb-3 border-secondary">
-            <img src="/images/additional_comments.jpg " alt="Additional Comments" className="card-img-top" />
+
               <div className="card-body">
-                <label htmlFor="additionalComments" className="form-label"><strong>11. Please let us know your other improvement ideas and suggestions </strong></label>
-                <p>உங்களது மற்ற முன்னேற்ற கருத்துக்கள் மற்றும் ஆலோசனைகளை எங்களுக்கு தெரியப்படுத்தவும்</p>
+                <img src="/images/additional_comments.jpg " alt="Additional Comments" className="card-img-top" />
+                <label htmlFor="additionalComments" className="form-label " style={{ fontSize: '0.9rem', marginBottom: '0.01rem', textTransform: "uppercase" }}><strong>Please let us know your other improvement ideas and suggestions  </strong>
+                  <p className='mt-1' style={{ fontSize: '0.8rem' }}>உங்களின் மற்ற மேம்பாட்டு யோசனைகள் மற்றும் பரிந்துரைகளை எங்களுக்குத் தெரிவிக்கவும்</p>
+                </label>
+
                 <textarea
                   className="form-control"
                   id="additionalComments"
@@ -292,6 +327,7 @@ const BreakfastFeedbackForm = () => {
             </div>
           </form>
         </div>
+        <label htmlFor="">Home</label>
       </div>
     </div>
   );
